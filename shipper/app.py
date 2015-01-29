24fafee8ccb14d15e36bc8bed97bbc0a4b6c13ce
steven jacobs <stjacobs@fastmail.fm>
2015-01-29 00:40:26 -0500

    change debug to false in master

--- shipper/app.py
+++ shipper/app.py
@@ -162,6 +162,6 @@ def show_rate():
                             rate_id=rate_id)
 
 if __name__ == '__main__':
-    app.debug = True
+    # app.debug = True
     # app.run(host='0.0.0.0')
     app.run()
